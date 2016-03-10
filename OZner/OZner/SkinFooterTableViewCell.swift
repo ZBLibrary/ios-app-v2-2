c83632b37c8da6ab5ede6ae26cd97c323fdba884
zhaowuxie <545691639@qq.com>
2016-03-11 01:05:44 +0800

    zb

--- OZner/OZner/SkinFooterTableViewCell.swift
+++ OZner/OZner/SkinFooterTableViewCell.swift
@@ -10,8 +10,20 @@ import UIKit
 
 class SkinFooterTableViewCell: UITableViewCell {
 
+    
+    @IBOutlet weak var widthOfButtonBG1: NSLayoutConstraint!
+    @IBOutlet weak var widthOfButtonBG2: NSLayoutConstraint!
+    @IBOutlet weak var widthOfButtonBG3: NSLayoutConstraint!
+    @IBOutlet weak var widthOfButtonBG4: NSLayoutConstraint!
+    @IBOutlet weak var widthOfButtonBG5: NSLayoutConstraint!
+    private let buttonContainWidth=(375-24)*Screen_Width/375
     override func awakeFromNib() {
         super.awakeFromNib()
+        widthOfButtonBG1.constant=0
+        widthOfButtonBG2.constant=buttonContainWidth/4
+        widthOfButtonBG3.constant=buttonContainWidth/4
+        widthOfButtonBG4.constant=buttonContainWidth/4
+        widthOfButtonBG5.constant=buttonContainWidth/4
         // Initialization code
     }
 
