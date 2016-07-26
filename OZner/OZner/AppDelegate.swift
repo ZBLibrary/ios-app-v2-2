29f08f72e5cd0d81f75529bb65bbb8de414a8c70
婉卿容若 <devzheng@qq.com>
2016-07-26 17:49:29 +0800

    no message

--- OZner/OZner/AppDelegate.swift
+++ OZner/OZner/AppDelegate.swift
@@ -27,7 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate ,WXApiDelegate,UIAlertView
         self.window?.rootViewController = switchController;
         self.window?.makeKeyAndVisible()
         
-        //
+        
         
         //注册微信
         //WXApi.registerApp("wx45a8cc642a2295b5", withDescription: "haoze")
