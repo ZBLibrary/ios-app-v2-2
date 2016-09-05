33b5c2b14bc669da8aaf6fc1876a430c89c0141e
zgy <zhuguangyang@zgy.local>
2016-09-05 09:52:37 +0800

    //修改引导图，免责条款等

--- OZner/OZner/SwitchViewController.m
+++ OZner/OZner/SwitchViewController.m
@@ -103,7 +103,7 @@
     
     [self loadFirstView];
     
-    //[self isFirstOpenApp];
+    [self isFirstOpenApp];
     
 }
 
--- OZner/OZner/userAgreeMentsController.swift
+++ OZner/OZner/userAgreeMentsController.swift
@@ -9,14 +9,14 @@
 import UIKit
 
 class userAgreeMentsController: UIViewController {
-
-
+    
+    
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?){
         
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         
     }
-        required init(coder aDecoder: NSCoder) {
+    required init(coder aDecoder: NSCoder) {
         
         fatalError("init(coder:) has not been implemented")
         
@@ -29,25 +29,26 @@ class userAgreeMentsController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //免费条款
         webView.loadRequest(NSURLRequest(URL: NSURL(string: "http://cup.ozner.net/app/cn/mzsm.html")!))
         webView.scalesPageToFit = true
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
